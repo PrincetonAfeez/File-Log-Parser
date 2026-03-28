feb47846df9b6ccef21347ed4452f3468f75c478
PrincetonAfeez <newlifewithprince@live.com>
2026-03-28 09:46:31 -0700

    10 clean commits to establish basic structure

--- parser.py
+++ parser.py
@@ -5,3 +5,5 @@ feat: implement safe file reading logic
 feat: add initial regex for IP and status code extraction
 
 feat: implement status code frequency counting
+
+feat: implement IP hit tracking
